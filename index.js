4792ef958281b0385a32f5ff11e5762a8fbc68ce
vijayakumarBit <vijayakumar2231@gmail.com>
2018-12-10 11:04:57 +0000

    Mongo connected

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+const db_url = 'mongodb://kirio:kirio1234@ds221990.mlab.com:21990/kiri';
+var main_db;
+app.use(express.json());
 
-app.get('/list',(req,res)=>{
-	res.send('List getting..');
+app.get('/', (req, res) => {
+	res.status(200).send('Main Server working at 3000');
+});
+app.post('/addtodb',(req,res)=>{
+	console.log(req);
+});
+MongoClient.connect(db_url,{ useNewUrlParser: true },(err,database)=>{
+	if(err){
+		return console.log('DB error:' + err);
+	}
+	main_db=database;
+	app.listen(6200, () => {
+	console.log('Port Listing 6200 .');
+});
+	
 });
 
-app.listen(3000,()=>{
-	console.log('Port Listing 3000 .');
-});
