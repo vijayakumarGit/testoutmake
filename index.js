188006b81a251742eecd18ff905471dc4d8561c7
vijayakumarGit <vijayakumar2231@gmail.com>
2018-12-20 12:04:42 +0000

    add allow origin

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const router= require('./routes/routers');
 app.use(express.json());
 app.use(router);
 
+// Add headers
+app.use((req, res, next)=> {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // app.get('/', (req, res) => {
 // 	res.status(200).send('Main Server working at 3000');
 // });
